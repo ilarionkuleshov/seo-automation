e59327c738ac4c94b11599da55ac20a6d3bc46fe
ilarionkuleshov <ilarion.kuleshov@gmail.com>
2025-05-15 14:57:02 +0300

    fix: rename image component module

--- src/components/__init__.py
+++ src/components/__init__.py
@@ -1,4 +1,4 @@
 from .card import card_grid
 from .gsheet import gsheet_selector
-from .images import example_image
+from .image import example_image
 from .status import progress_status, return_stage_context, stage_status
